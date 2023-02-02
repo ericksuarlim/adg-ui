304587de21cbae1852800978fe9dfd7709a7c0ea
ericksuarlim <60761434+ericksuarlim@users.noreply.github.com>
2023-02-02 18:03:18 -0400

    Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDwdp0E8BA5bLXad5-rPRXymTHFTuPJ-RU",
+    authDomain: "adg-ui.firebaseapp.com",
+    projectId: "adg-ui",
+    storageBucket: "adg-ui.appspot.com",
+    messagingSenderId: "600192451808",
+    appId: "1:600192451808:web:e45db6262c59047047fc85"
+  },
   urlApi: "https://adg-api.onrender.com"
 
 };
